cb2ebae0f4bd13455b1f708fa182c836abc7b4b0
ICE <donfeng97@csu.fullerton.edu>
2020-03-18 21:05:29 -0700

    Add files via upload

---
+++ question3.php
@@ -0,0 +1,18 @@
+<?php
+$row = mysqli_fetch_object($result);
+$db_userid = $row->admin_id;
+$db_password = $row→admin_password;
+$encryptpasswd = sha1($userPasswd); // Note: sha1 encryption is obsolete
+$db_name = $row->admin_name;
+if ($db_userid != $userid || $db_password != $encryptpasswd) {
+    // TODO
+    $query = "INSERT INTO administrator(admin_id, admin_password, admin_name)
+    VALUES('$userid', '$encryptpasswd', '$name')";
+    mysqli_query($connection, $query);
+
+    if (!$result) {
+        echo ("Insert to administrator failed. " . mysqli_error($connection));
+        exit();
+    }
+}
+?>
